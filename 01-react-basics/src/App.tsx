21577c87ae63c39341a2dc67382f6a608f521e3c
Harald <Harald@espinagar.se>
2023-04-25 14:39:20 +0200

    render out list of post with map

--- 01-react-basics/src/App.tsx
+++ 01-react-basics/src/App.tsx
@@ -1,19 +1,35 @@
 import { useState } from 'react'
 import './App.css'
 
-const App = () => {
-  
-  let [msg, setMsg] = useState("I'm stateful")
-  let [count, setCount] = useState(0)
+type Post = {
+  id: number,
+  title: string,
+  likes: number,
+}
 
-  const handleButtonClick = () => {
-    console.log("You clicked the button, good boi")
+const App = () => {
+
+const [msg, setMsg] = useState("I'm stateful")
+const [count, setCount] = useState(0)
+const [posts, setPosts] = useState<Post[]>([
+  { id: 1, title: "React Rocks🤟!", likes: 1337 },
+  { id: 2, title: "JSX Rocks Even More☠️!", likes: 1534 },
+  { id: 3, title: "Got state?🧐", likes: 8923 },
+])
+
+const handleButtonClick = () => {
+  console.log("You clicked the button, good boi")
     setMsg("State has been changed")
     setCount(count + 1)
   }
+  
+  console.table(posts)
+const done = () => {
+  setMsg('React is wonderful')
+}
 
   return (
-  <div className='App'>
+    <div className='App'>
     <h1>React Basics</h1>
 
     <h2>{msg}</h2>
@@ -21,6 +37,20 @@ const App = () => {
     <p>You have clicked the button {count} times.</p>
 
     <button onClick={handleButtonClick} className='btn btn-success btn-lg'>👆 me!</button>
+
+    <button onClick={done} className='btn btn-danger btn-lg'>Done</button>
+
+    <h2>Posts</h2> 
+
+    <ul>
+      {
+        posts.map(post => (
+            <li key={post.id}> 
+              {post.title} ({post.likes} Likes)
+            </li>
+          ))
+      }
+    </ul>
   </div>
   )
 }
