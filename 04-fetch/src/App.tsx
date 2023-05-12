50d4655da023e0f091271c5869792a9ff398a060
Harald <Harald@espinagar.se>
2023-05-12 15:22:23 +0200

    redone and moved components

--- 04-fetch/src/App.tsx
+++ 04-fetch/src/App.tsx
@@ -1,42 +1,43 @@
 import { useEffect, useState } from 'react'
+import { getResource } from './services/API'
+import { IResource } from './types'
 import './assets/scss/App.scss'
-
-interface IResource {
-	id: number,
-	title: string
-}
+import ResourceList from './components/ResourceList'
 
 function App() {
-	const [resource, setResource] = useState('posts')
+	const [resource, setResource] = useState('')
 	const [data, setData] = useState<IResource[]>([])
-	const [state, setState] = useState('')
-	const [isPending, setIsPending] = useState(false)
-
-	
-	
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState('')
 
 	useEffect(() => {
-		if (!resource) {
-			return
+		const fetchData = async () => {
+			if (!resource) {
+				return
+			}
+
+			// empty data & error before fetching new
+			// and set loading
+			setError('')
+			setData([])
+			setLoading(true)
+
+			try {
+				const payload = await getResource(resource)
+
+				// update data state with resource payload
+				setData(payload)
+				setLoading(false)
+
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			} catch (e: any) {
+				setError(e.toString())
+				setLoading(false)
+		
+			}
 		}
 
-		const fetchData = async () => {		
-			setIsPending(true)
-			// fetch resource	
-			const res = await fetch(`https://jsonplaceholder.typicode.com/${resource}`)
-
-			// parse response as json
-			const payload = await res.json() as IResource[]
-
-			setIsPending(false)
-
-			// update data state with resource payload 
-			setData(payload)
-			setState(resource)
-
-		}
-
-		// call function 
+		// call function
 		fetchData()
 	}, [resource])
 
@@ -44,29 +45,20 @@ function App() {
 		<div className="container">
 			<h1 className="mb-3">Fetch</h1>
 
-			<div className="d-flex justify-content-between">
-				<button onClick={() => setResource('Albums')} className="btn btn-primary">Albums</button>
+			<div className="d-flex justify-content-between mb-3">
+				<button onClick={() => setResource('albums')} className="btn btn-primary">Albums</button>
 				<button onClick={() => setResource('photos')} className="btn btn-success">Photos</button>
 				<button onClick={() => setResource('posts')} className="btn btn-warning">Posts</button>
 				<button onClick={() => setResource('todos')} className="btn btn-danger">Todos</button>
+				<button onClick={() => setResource('memes')} className="btn btn-info">Memes 😂</button>
 			</div>
 
-			{isPending && (
-				<p>Pending...</p>
-			)}
-
-			{resource && (
-				<>
-					<h2>{state}</h2>
-					<p>There are {data.length} {state}.</p>
-					<ol>
-						{data.map(item => (
-							<li key={item.id}>{item.title}</li>
-						))}
-					</ol>
-				</>
-			)}
-		</div>
+			<ResourceList
+				error={error}
+				loading={loading}
+				resource={resource}
+				data={data}
+			/>		</div>
 	)
 }
 
---
+++ 04-fetch/src/components/ResourceList.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { IResource } from '../types'
+
+interface Iprops {
+    error: string,
+    loading: boolean,
+    resource: string,
+    data: IResource[]
+}
+
+const ResourceList: React.FC<Iprops> = ({error, loading, resource, data}) => {
+  
+    if (loading) {
+		return <p>Loading...</p>
+	}
+
+	if (error) {
+		return <p>{error}</p>
+	}
+
+    if (!resource) {
+        return <p>Please select a resource to view</p>
+    }
+
+    if (!data.length) {
+        return <p>No data existed</p>
+    }
+
+    return (
+    <>
+
+            <h2>{resource}</h2>
+            <p>There are {data.length} {resource}.</p>
+            <ol>
+                {data.map(item => (
+                    <li key={item.id}>{item.title}</li>
+                ))}
+            </ol>
+        </>
+    )
+  
+}
+
+export default ResourceList
