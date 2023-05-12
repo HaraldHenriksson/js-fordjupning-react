d1598038ad2fbdfee6108dfa9b7f5915aabc8f67
Harald <Harald@espinagar.se>
2023-05-12 13:08:48 +0200

    resource not showing before data done

--- 04-fetch/src/App.tsx
+++ 04-fetch/src/App.tsx
@@ -9,8 +9,16 @@ interface IResource {
 function App() {
 	const [resource, setResource] = useState('posts')
 	const [data, setData] = useState<IResource[]>([])
+	const [state, setState] = useState('')
+
+	
+	
 
 	useEffect(() => {
+		if (!resource) {
+			return
+		}
+
 		const fetchData = async () => {		
 			// fetch resource	
 			const res = await fetch(`https://jsonplaceholder.typicode.com/${resource}`)
@@ -20,6 +28,8 @@ function App() {
 
 			// update data state with resource payload 
 			setData(payload)
+			setState(resource)
+
 		}
 
 		// call function 
@@ -31,16 +41,16 @@ function App() {
 			<h1 className="mb-3">Fetch</h1>
 
 			<div className="d-flex justify-content-between">
-				<button onClick={() => setResource('albums')} className="btn btn-primary">Albums</button>
+				<button onClick={() => setResource('Albums')} className="btn btn-primary">Albums</button>
 				<button onClick={() => setResource('photos')} className="btn btn-success">Photos</button>
 				<button onClick={() => setResource('posts')} className="btn btn-warning">Posts</button>
 				<button onClick={() => setResource('todos')} className="btn btn-danger">Todos</button>
 			</div>
 
-			{data && (
+			{resource && (
 				<>
-					<h2>{resource}</h2>
-					<p>There are {data.length} {resource}.</p>
+					<h2>{state}</h2>
+					<p>There are {data.length} {state}.</p>
 					<ol>
 						{data.map(item => (
 							<li key={item.id}>{item.title}</li>
