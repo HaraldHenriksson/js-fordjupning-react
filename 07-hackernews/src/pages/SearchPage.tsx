53075c3aafb0c3b154db9667f5a82ad4be06c5c1
Harald <Harald@espinagar.se>
2023-06-02 15:11:12 +0200

    search page skeleton

--- 07-hackernews/src/pages/SearchPage.tsx
+++ 07-hackernews/src/pages/SearchPage.tsx
@@ -8,7 +8,7 @@ const SearchPage = () => {
         <>
             <h1>🔎🔦👀</h1>
 
-            <Form>
+            <Form className='mb-3'>
                 <Form.Group className="mb-3" controlId="searchQuery">
                     <Form.Label>Search Query</Form.Label>
                     <Form.Control
@@ -21,6 +21,44 @@ const SearchPage = () => {
                     <Button variant="success" type="submit">Search</Button>
                 </div>
             </Form>
+
+            {false && (
+                <p>🤔 Loading...</p>
+            )}
+            {true && (
+                <div id='search-result'>
+                    <p>Showing HITS search results for QUERY...</p>
+
+                    <ListGroup className='mb-3'>
+                        {[{}, {}].map(hit => (
+                            <ListGroup.Item
+                                action
+                                href={''}
+                                key={''}
+                            >
+                                <h2 className='h3'>TITLE</h2>
+                                <p className='text-muted-small-mb-0'>POINTS Posted at CREATED_AT by AUTHOR</p>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+
+                    <div className="d-flex justify-content-between align-items-center">
+                        <div className="prev">
+                            <Button
+                                variant='primary'
+                            >Previous Page</Button>
+                        </div>
+
+                        <div className="page">PAGE</div>
+
+                        <div className="next">
+                            <Button
+                                variant='primary'
+                            >Next Page</Button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
