c239064929ca172f6f3c958b0393cf44ace62d00
Harald <Harald@espinagar.se>
2023-06-05 10:08:29 +0200

    hackernewsapi, types and fetch

--- 07-hackernews/src/pages/SearchPage.tsx
+++ 07-hackernews/src/pages/SearchPage.tsx
@@ -2,34 +2,56 @@ import { useEffect, useRef, useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import ListGroup from 'react-bootstrap/ListGroup'
+import { searchByDate as HN_searchByDate } from '../services/HackerNewsAPI'
+import { HN_SearchResponse } from '../types'
 
 const SearchPage = () => {
     const [error, setError] = useState<string | null>(null)
     const [loading, setLoading] = useState(false)
     const [searchInput, setSearchInput] = useState("")
+    const [searchResult, setSearchResult] = useState<HN_SearchResponse | null>(null)
+
+    const searchHackerNews = async (searchQuery: string) => {
+        setError(null)
+        setLoading(true)
+        setSearchResult(null)
+
+        try {
+            const res = await HN_searchByDate(searchQuery)
+            setSearchResult(res)
+
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (err: any) {
+            setError(err.message)
+        }
+
+        setLoading(false)
+    }
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault()
 
+        // haxx0r
         if (!searchInput.trim().length) {
             return
         }
 
-
+        // search HN
+        searchHackerNews(searchInput)
     }
 
     return (
         <>
             <h1>🔎🔦👀</h1>
 
-            <Form className='mb-3' onSubmit={handleSubmit}>
+            <Form className="mb-4" onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="searchQuery">
                     <Form.Label>Search Query</Form.Label>
                     <Form.Control
                         onChange={e => setSearchInput(e.target.value)}
-                        type="text"
-                        required
                         placeholder="Enter your search query"
+                        required
+                        type="text"
                         value={searchInput}
                     />
                 </Form.Group>
@@ -43,22 +65,23 @@ const SearchPage = () => {
                 </div>
             </Form>
 
-            {false && (
-                <p>🤔 Loading...</p>
-            )}
-            {true && (
-                <div id='search-result'>
-                    <p>Showing HITS search results for QUERY...</p>
+            {loading && <p>🤔 Loading...</p>}
 
-                    <ListGroup className='mb-3'>
-                        {[{}, {}].map(hit => (
+            {searchResult && (
+                <div id="search-result">
+                    <p>Showing {searchResult.nbHits} search results for {searchInput}...</p>
+
+                    <ListGroup className="mb-3">
+                        {searchResult.hits.map(hit => (
                             <ListGroup.Item
                                 action
-                                href={''}
-                                key={''}
+                                href={hit.url}
+                                key={hit.objectID}
                             >
-                                <h2 className='h3'>TITLE</h2>
-                                <p className='text-muted-small-mb-0'>POINTS Posted at CREATED_AT by AUTHOR</p>
+                                <h2 className="h3">{hit.title}</h2>
+                                <p className="text-muted small mb-0">
+                                    {hit.points} points by {hit.author} at {hit.created_at}
+                                </p>
                             </ListGroup.Item>
                         ))}
                     </ListGroup>
@@ -66,7 +89,7 @@ const SearchPage = () => {
                     <div className="d-flex justify-content-between align-items-center">
                         <div className="prev">
                             <Button
-                                variant='primary'
+                                variant="primary"
                             >Previous Page</Button>
                         </div>
 
@@ -74,7 +97,7 @@ const SearchPage = () => {
 
                         <div className="next">
                             <Button
-                                variant='primary'
+                                variant="primary"
                             >Next Page</Button>
                         </div>
                     </div>
