6679df3f89b6527e9fd5b6da0ded88738321c398
Harald <Harald@espinagar.se>
2023-08-15 10:54:38 +0200

    10-react-query

---
+++ 10-react-query/src/App.tsx
@@ -0,0 +1,17 @@
+import Container from 'react-bootstrap/Container';
+
+import './assets/scss/App.scss'
+
+const App = () => {
+
+  return (
+    <>
+      <Container className='py-3'>
+        <h1>React Query</h1>
+      </Container>
+
+    </>
+  )
+}
+
+export default App
