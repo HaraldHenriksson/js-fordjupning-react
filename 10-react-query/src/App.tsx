2231dd2181a7607eb4688faa70721fe2b113ab5e
Harald <Harald@espinagar.se>
2023-08-15 11:04:39 +0200

    navbar

--- 10-react-query/src/App.tsx
+++ 10-react-query/src/App.tsx
@@ -1,15 +1,18 @@
 import Container from 'react-bootstrap/Container';
 
 import './assets/scss/App.scss'
+import Navigation from './pages/partials/Navigation';
 
 const App = () => {
 
   return (
     <>
-      <Container className='py-3'>
-        <h1>React Query</h1>
-      </Container>
-
+      <div id='App'>
+        <Navigation />
+        <Container className='py-3'>
+          <h1>React Query</h1>
+        </Container>
+      </div>
     </>
   )
 }
