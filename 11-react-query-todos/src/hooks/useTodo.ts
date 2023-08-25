e19b3dfb21f0ed991e57891d4055bd1585ddc495
Harald <Harald@espinagar.se>
2023-08-25 10:46:59 +0200

    Optional parameter for enabling and desabling query

--- 11-react-query-todos/src/hooks/useTodo.ts
+++ 11-react-query-todos/src/hooks/useTodo.ts
@@ -1,8 +1,8 @@
 import { useQuery } from '@tanstack/react-query'
 import { getTodo as TodosAPI_getTodo } from '../services/TodosAPI'
 
-const useTodo = ( todoId: number ) => {
-	return useQuery(["todo", { id: todoId }], () => TodosAPI_getTodo(todoId))
+const useTodo = ( todoId: number, enabled = true ) => {
+	return useQuery(["todo", { id: todoId }], () => TodosAPI_getTodo(todoId), {enabled, })
 }
 
 export default useTodo
