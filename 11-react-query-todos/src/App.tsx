18d963c1687b9a325620d260636ddbebc044cb6f
Harald <Harald@espinagar.se>
2023-08-25 12:40:05 +0200

    toastify not working

--- 11-react-query-todos/src/App.tsx
+++ 11-react-query-todos/src/App.tsx
@@ -1,6 +1,7 @@
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import Container from 'react-bootstrap/Container'
 import { Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-bootstrap'
 import GlobalLoadingSpinner from './components/GlobalLoadingSpinner'
 import Navigation from './components/Navigation'
 import CreateTodoPage from './pages/CreateTodoPage'
@@ -16,6 +17,7 @@ const App = () => {
 		<div id="App">
 			<Navigation />
 			<GlobalLoadingSpinner />
+			<ToastContainer />
 
 			<Container className="py-3">
 				<Routes>
--- 11-react-query-todos/src/pages/HomePage.tsx
+++ 11-react-query-todos/src/pages/HomePage.tsx
@@ -1,9 +1,34 @@
+import Button from "react-bootstrap/Button"
+import ButtonGroup from "react-bootstrap/ButtonGroup"
+import { toast } from "react-toastify"
+
 const HomePage = () => {
 	return (
 		<>
 			<h1>Welcome to Better Todos!</h1>
 
-			<p>This text does not exist.</p>
+			<ButtonGroup>
+				<Button
+					variant="primary"
+					onClick={() =>
+						toast("Wow such click much wow very celebrate")
+					}
+				>
+					Celebrate!
+				</Button>
+
+				<Button
+					variant="success"
+					onClick={() => {
+						toast.success(
+							"Wow such success, very influcencer, much money!"
+						)
+					}}
+				>
+					Click me!
+				</Button>
+
+			</ButtonGroup>
 		</>
 	)
 }
