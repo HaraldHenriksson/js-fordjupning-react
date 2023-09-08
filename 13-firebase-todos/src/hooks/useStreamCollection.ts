396c5c084c7a49eb96a634c2602d9f97ed1830dd
Harald <Harald@espinagar.se>
2023-09-08 11:35:43 +0200

    order by completed and title

--- 13-firebase-todos/src/hooks/useStreamCollection.ts
+++ 13-firebase-todos/src/hooks/useStreamCollection.ts
@@ -1,14 +1,20 @@
-import { CollectionReference, onSnapshot } from 'firebase/firestore'
+import { CollectionReference, QueryConstraint, onSnapshot, query } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
 
-const useStreamCollection  = <T>(colRef: CollectionReference<T>) => {
+const useStreamCollection  = <T>(
+    colRef: CollectionReference<T>,
+    ...QueryConstraint: QueryConstraint[]
+    ) => {
 	const [data, setData] = useState<T[]|null>(null)
 	const [loading, setLoading] = useState(true)
 
 	// Get data on component mount
 	useEffect(() => {
+        // construct a query reference 
+        const queryRef = query(colRef, ...QueryConstraint)
+        
         // Subscribe to the changes in the collection
-        const unsubscribe = onSnapshot(colRef, (snapshot) => {
+        const unsubscribe = onSnapshot(queryRef , (snapshot) => {
             // loop over all docs
             const data: T[] = snapshot.docs.map(doc => {
                 return {
