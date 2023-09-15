76ca935200d05c8b6699efae77bbe34b0c2546b4
Harald <Harald@espinagar.se>
2023-09-15 11:55:09 +0200

    app setup

---
+++ 14-instamemes/src/components/UploadMeme.tsx
@@ -0,0 +1,13 @@
+import { useCallback } from 'react'
+import classNames from 'classnames'
+import { useDropzone } from 'react-dropzone'
+
+const UploadMeme = () => {
+
+
+    return (
+        <div>UploadMeme</div>
+    )
+}
+
+export default UploadMeme
--- 14-instamemes/src/pages/HomePage.tsx
+++ 14-instamemes/src/pages/HomePage.tsx
@@ -1,16 +1,18 @@
 import Container from "react-bootstrap/Container"
+import UploadMeme from "../components/UploadMeme"
 import useAuth from "../hooks/useAuth"
 
 const HomePage = () => {
 	const { currentUser } = useAuth()
-	if (!currentUser) {
-		throw new Error("YOU CAN BE A USER")
-	}
 
 	return (
 		<Container className="py-3">
 			<h1>InstaMemes</h1>
 			<h2>When I get sad I stop being sad and be awesome instead</h2>
+
+			{currentUser && <UploadMeme />}
+
+			<p>Here be all dem memes...</p>
 
 		</Container>
 	)
